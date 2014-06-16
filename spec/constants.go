8062ffef2532b5f9fe733109f12ef427bd972b17
Peter Olsen <pto@me.com>
2014-06-16 19:27:21 -0400

    more constants

--- spec/constants.go
+++ spec/constants.go
@@ -1,9 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// Unusual, but valid literal
 	one := 00000000001
 	fmt.Println("00000000001 equals", one)
+
+	var (
+		ui8  uint8      = 3.0
+		i8   int8       = 3.0
+		ui16 uint16     = 3.0
+		i16  int16      = 3.0
+		ui32 uint32     = 3.0
+		i32  int32      = 3.0
+		ui64 uint64     = 3.0
+		i64  int64      = 3.0
+		f32  float32    = 3.0
+		f64  float64    = 3.0
+		c64  complex64  = 3.0
+		c128 complex128 = 3.0
+	)
+
+	fmt.Println(ui8, i8, ui16, i16, ui32, i32, ui64, i64, f32, f64, c64, c128)
+
+	ui32 = 2147483648.0
+	f32 = 2147483648.0
+	f64 = 2147483648.0
+	// i32 = 2147483648.0 // error
+
+	fmt.Println(1<<31, ui32, f32, f64 /* i32 */)
+
+	fmt.Println("Largest integer constant is bigger than", float64(1<<255))
+	const (
+		start      = 1 << 255
+		next       = start + 1
+		difference = start - next
+	)
+	fmt.Println("The difference between two huge consts is", difference)
+
+	fmt.Println("Largest floating point constant is bigger than 2 to the power of", 1<<31)
+	fmt.Println("which is about", math.Pow(2, 1<<31))
 }
