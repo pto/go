4c22bb169001121c82af9b6c729fa0f14c6e2f4e
Peter Olsen <pto@me.com>
2014-12-06 09:04:34 -0500

    showing slice contents

--- spec/slice_inside.go
+++ spec/slice_inside.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	s := make([]int, 8)
 	fmt.Println("unsafe.Sizeof(s) is", unsafe.Sizeof(s))
+	p := unsafe.Pointer(&s)
+	b := (*[unsafe.Sizeof(s)]byte)(p)
+	fmt.Printf("contents of s is % #x\n", b)
 }
