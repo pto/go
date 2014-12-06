f341620737df61c21edecc484da606ad7f18a611
Peter Olsen <pto@me.com>
2014-12-06 09:17:54 -0500

    more action

--- spec/slice_inside.go
+++ spec/slice_inside.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	s := make([]int, 1, 2)
+	fmt.Println("s is", s)
 	fmt.Println("len(s) is", len(s), "and cap(s) is", cap(s))
 	fmt.Println("unsafe.Sizeof(s) is", unsafe.Sizeof(s))
 	p := unsafe.Pointer(&s)
 	b := (*[unsafe.Sizeof(s)]byte)(p)
-	fmt.Printf("contents of s is % #x\n", *b)
+	fmt.Printf("contents of s before append is % #x\n", *b)
+	s = append(s, 123)
+	fmt.Printf("contents of s after append is % #x\n", *b)
+	fmt.Println("s is", s)
 }
