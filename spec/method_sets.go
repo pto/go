344d621f2a0b3117e29cbab5a199b72e98105b7b
Peter Olsen <pto@me.com>
2015-02-14 18:58:41 -0500

    method sets and pointers

---
+++ spec/method_sets.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type T int
+
+func (t T) FuncOfT() {
+	fmt.Println("FuncOfT()")
+}
+
+func (t *T) FuncOfPT() {
+	fmt.Println("FuncOfPT()")
+}
+
+type IT interface {
+	FuncOfT()
+}
+
+type IPT interface {
+	FuncOfPT()
+}
+
+func main() {
+	t1 := T(42)
+	t2 := new(T)
+	*t2 = 123
+	fmt.Printf("t1: %[1]v %[1]T\n", t1, t1)
+	fmt.Printf("t2: %[1]v %[1]T\n", t2, t2)
+	fmt.Printf("*t2: %[1]v %[1]T\n", *t2, *t2)
+
+	var it IT
+	var ipt IPT
+	it = t1 // Obviously OK
+	fmt.Printf("it: %[1]v %[1]T\n", it, it)
+	it = t2 // Method set of *T contains method set of T
+	fmt.Printf("it: %[1]v %[1]T\n", it, it)
+	fmt.Printf("*(it.(*T)): %[1]v %[1]T\n", *(it.(*T)), *(it.(*T)))
+	// ipt = t1 // WRONG: Method set of T does not contain method set of *T
+	ipt = t2 // Obviously OK
+	fmt.Printf("ipt: %[1]v %[1]T\n", ipt, ipt)
+	fmt.Printf("*(ipt.(*T)): %[1]v %[1]T\n", *(ipt.(*T)), *(ipt.(*T)))
+}
---
+++ spec/slice_index_order.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := make([][]int, 1)
+	x[0] = make([]int, 2)
+	x[0][0] = 1
+	x[0][1] = 2
+	fmt.Println(x)
+
+	// A different way to initialize
+	y := make([][]int, 0)
+	y = append(y, make([]int, 1))
+	y = append(y, make([]int, 1))
+	y[0][0] = 10
+	y[1][0] = 11
+	fmt.Println(y)
+
+	// Or even this
+	y[0] = append(y[0], 42)
+	fmt.Println(y)
+	fmt.Println(y[0][1])
+}
