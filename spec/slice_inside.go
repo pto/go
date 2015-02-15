6e15e1ae143b3f337d8f12b7cc9662644f481e34
Peter Olsen <pto@me.com>
2015-02-14 20:11:09 -0500

    minor cleanup

--- spec/slice_inside.go
+++ spec/slice_inside.go
@@ -5,9 +5,10 @@ import (
 	"unsafe"
 )
 
+// Shows the internals of a slice.
 func main() {
 	s := make([]int, 1, 2)
-	fmt.Println("ACTION: make([]int, 1, 2)")
+	fmt.Println("ACTION: s := make([]int, 1, 2)")
 	var i int
 	fmt.Println("unsafe.Sizeof(s) is", unsafe.Sizeof(s),
 		"which is 3 times", unsafe.Sizeof(i), "(unsafe.Sizeof(int))")
@@ -19,18 +20,18 @@ func main() {
 	fmt.Printf("contents of s is % #x\n", *bs)
 
 	s = append(s, 123)
-	fmt.Println("ACTION: append(s, 123)")
+	fmt.Println("ACTION: s = append(s, 123)")
 	fmt.Println("s is", s, "with len of", len(s), "and cap of", cap(s))
 	fmt.Printf("contents of s is % #x\n", *bs)
 
 	s = append(s, 456)
-	fmt.Println("ACTION: append(s, 456)")
+	fmt.Println("ACTION: s = append(s, 456)")
 	fmt.Println("s is", s, "with len of", len(s), "and cap of", cap(s))
 	fmt.Printf("contents of s is % #x\n", *bs)
 
 	var a [3]int
 	sa := a[:]
-	fmt.Println("ACTION: sa = a[:]")
+	fmt.Println("ACTION: sa := a[:]")
 	psa := unsafe.Pointer(&sa)
 	bsa := (*[unsafe.Sizeof(sa)]byte)(psa)
 	fmt.Println("sa is", sa, "with len of", len(sa), "and cap of", cap(sa))
