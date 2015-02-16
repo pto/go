e66e9370e19e9d7d3f4bf5c3966bb2d0cc44cc11
Peter Olsen <pto@me.com>
2015-02-16 14:32:47 -0500

    improving on struct embedded fields

--- spec/structs.go
+++ spec/structs.go
@@ -5,12 +5,12 @@ import "fmt"
 // T is a simple type for embedding
 type T int64
 
-func (t T) doIt() {
-	fmt.Println("doIt:", t)
+func (t T) Do() {
+	fmt.Println("Do:", t)
 }
 
-func (t *T) ptrDoIt() {
-	fmt.Println("ptrDoIt:", *t)
+func (t *T) PtrDo() {
+	fmt.Println("PtrDo:", *t)
 }
 
 // S embeds T
@@ -18,58 +18,73 @@ type S struct {
 	T
 }
 
-// S2 embeds *T
-type S2 struct {
+// SP embeds *T
+type SP struct {
 	*T
 }
 
-// I is a doIter
-type I interface {
-	doIt()
+type Doer interface {
+	Do()
 }
 
-// PI is a ptrDoIter
-type PI interface {
-	ptrDoIt()
+type PtrDoer interface {
+	PtrDo()
 }
 
+// Shows that method sets of non-pointer objects only include non-pointer
+// methods, but method sets of pointer objects include both pointer and
+// non-pointer methods.
 func main() {
-	var s S
-	var ps = &s
-	s.T = 123
-	fmt.Println("values of T:", s.T, ps.T)
-	fmt.Println("calls on s and ps:")
-	s.doIt()
-	s.ptrDoIt() // automatic address of (even though not in method set of S)
-	ps.doIt()   // automatic dereference
-	ps.ptrDoIt()
-
-	var x I
-	var px PI
-	x = s  // ok: method set of S includes promoted method with receiver T
-	x = ps // ok: method set of *S includes promoted method with receiver T
-	// px = s  // not ok: method set of S does not include promoted method with receiver *T
-	px = ps // ok: method set of *S includes promoted method with receiver *T
-	fmt.Println("calls on interface variables:")
-	x.doIt()
-	px.ptrDoIt()
-
-	var s2 S2
+	var s1, s2 S
+	s1.T = 111
+	s2.T = 222 // automatic dereference to use field
 	var ps2 = &s2
-	s2.T = new(T)
-	*(s2.T) = 456
-	fmt.Println("values of *T:", *s2.T, *ps2.T)
-	fmt.Println("calls on s2 and ps2")
-	s2.doIt() // automatic dereference of *T
-	s2.ptrDoIt()
-	ps2.doIt()    // double dereference of ps2 and *T
-	ps2.ptrDoIt() // automatic deference of ps2
+	fmt.Println("s1.T:", s1.T)   // normal use of embedded field
+	fmt.Println("ps2.T:", ps2.T) // automatic dereference to use field
+	fmt.Println("calls on s1 and ps1:")
+	s1.Do()     // normal call of non-pointer method
+	s1.PtrDo()  // automatic address of (even though not in method set of S)
+	ps2.Do()    // automatic dereference
+	ps2.PtrDo() // normal call of pointer method
+	fmt.Println()
 
-	x = s2
-	x = ps2
-	px = s2
-	px = ps2
+	var i Doer
+	var pi PtrDoer
 	fmt.Println("calls on interface variables:")
-	x.doIt()
-	px.ptrDoIt()
+	i = s1 // ok: method set of S includes promoted method with receiver T
+	i.Do()
+	i = ps2 // ok: method set of *S includes promoted method with receiver T
+	i.Do()
+	// pi = s1 // not ok: method set of S does not include promoted method with receiver *T
+	// pi.PtrDo()
+	pi = ps2 // ok: method set of *S includes promoted method with receiver *T
+	pi.PtrDo()
+	fmt.Println()
+
+	var sp1, sp2 SP
+	sp1.T = new(T)
+	*(sp1.T) = 333
+	sp2.T = new(T)
+	*(sp2.T) = 444
+	var psp2 = &sp2
+	fmt.Println("*sp1.T:", *sp1.T)   // normal dereferece of embedded pointer
+	fmt.Println("*psp2.T:", *psp2.T) // normal deref of auto deref
+	fmt.Println("calls on s2 and ps2")
+	sp1.Do()     // automatic dereference of *T
+	sp1.PtrDo()  // normal use of *T
+	psp2.Do()    // double dereference of psp2 and *T
+	psp2.PtrDo() // automatic deference of psp2
+	fmt.Println()
+
+	// All of these are OK because the method set of embedded field *T
+	// is available to both S and *S
+	fmt.Println("calls on interface variables:")
+	i = sp1
+	i.Do()
+	i = psp2
+	i.Do()
+	pi = sp1
+	pi.PtrDo()
+	pi = psp2
+	pi.PtrDo()
 }
