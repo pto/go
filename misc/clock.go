d4d4855d049f77f94c048054edbb9460fdcffd2f
Peter Olsen <pto@me.com>
2015-07-04 09:00:26 -0400

    show number of days

--- misc/clock.go
+++ misc/clock.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
 func main() {
-	location, err := time.LoadLocation("Local")
-	if err != nil {
-		log.SetFlags(log.Lshortfile)
-		log.Fatal(err)
-	}
-	target := time.Date(2015, 6, 21, 12, 37, 53, 0, location)
+	fmt.Println("\t\x1b[1;36mJust Go!\x1b[0m")
+	target := time.Date(2015, 7, 5, 0, 0, 0, 0, time.UTC)
 	fmt.Printf("\t%s\n", target.Format(time.UnixDate))
 
 	var previous time.Time
+	var days int64
 	for {
 		now := time.Now()
 		now = now.Add(time.Duration(-now.Nanosecond()))
 		if now != previous {
 			previous = now
 			remaining := -target.Sub(now)
-			sign := ""
-			if remaining >= 0 {
-				sign = "+"
+			sign := "+"
+			if remaining < 0 {
+				sign = "-"
+				remaining = -remaining
 			}
-			fmt.Printf("\t%s  %s%v              \r",
-				now.Format(time.UnixDate), sign, remaining)
+			if remaining >= 24*time.Hour {
+				days = int64(remaining / (24 * time.Hour))
+				remaining = remaining % (24 * time.Hour)
+			}
+			fmt.Printf("\t%s  %s", now.Format(time.UnixDate), sign)
+			if days > 0 {
+				fmt.Printf("%dd", days)
+			}
+			fmt.Printf("%v         \r", remaining)
 		}
 		time.Sleep(50 * time.Millisecond)
 	}
