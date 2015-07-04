823575c52e8dc271742362589d1f3adaefc8a79f
Peter Olsen <pto@me.com>
2015-07-04 13:51:00 -0400

    minor stylistic changes

--- misc/clock.go
+++ misc/clock.go
@@ -11,27 +11,27 @@ func main() {
 	fmt.Printf("\t%s\n", target.Format(time.UnixDate))
 
 	var previous time.Time
-	var days int64
+	var days int
 	for {
 		now := time.Now()
-		now = now.Add(time.Duration(-now.Nanosecond()))
+		now = now.Add(time.Duration(-now.Nanosecond())) // truncate to second
 		if now != previous {
 			previous = now
-			remaining := -target.Sub(now)
-			sign := "+"
+			remaining := target.Sub(now)
+			sign := "-"
 			if remaining < 0 {
-				sign = "-"
+				sign = "+" // now is after target
 				remaining = -remaining
 			}
 			if remaining >= 24*time.Hour {
-				days = int64(remaining / (24 * time.Hour))
+				days = int(remaining / (24 * time.Hour))
 				remaining = remaining % (24 * time.Hour)
 			}
 			fmt.Printf("\t%s  %s", now.Format(time.UnixDate), sign)
 			if days > 0 {
 				fmt.Printf("%dd", days)
 			}
-			fmt.Printf("%v         \r", remaining)
+			fmt.Printf("%v              \r", remaining)
 		}
 		time.Sleep(50 * time.Millisecond)
 	}
