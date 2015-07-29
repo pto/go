cd0f89506fc79e149cf4aa0472779c72d6c58f64
Peter Olsen <pto@me.com>
2015-07-28 21:39:40 -0400

    big comment

--- csp/process.go
+++ csp/process.go
@@ -15,6 +15,7 @@ type Event string
 type Events []Event
 
 func (events Events) String() string {
+	// Convert []Event to []String so we can use Join
 	s := make([]string, 0, len(events))
 	for _, e := range events {
 		s = append(s, string(e))
@@ -77,7 +78,43 @@ var VMC Process
 
 func init() {
 	// VMC is a closure of itself, so it can be defined recursively
-	VMC = func(e Event) Process { return VMC(e) }
+	VMC = func(e Event) Process {
+		fmt.Println("calling VMC closure for event", e)
+		return VMC(e)
+	}
+	// At this point the variable VMC holds a value that is a function
+	// containing a closure over the variable VMC. When this
+	// function runs, it will evaluate the current value of the VMC
+	// variable and call it.
+	//
+	// There is, therefore, a difference between the value of VMC at this
+	// point in the code execution (the closure of VMC over itself), and
+	// the value of VMC as it will be when the function value from the
+	// previous line is actually executed (the more complicated VMC that
+	// will be defined next).
+	//
+	// When the next line of code runs, it will evaluated the current
+	// value of VMC (the closure of VMC over itself) and pass that value
+	// to Choice2 and Prefix. That value, like all non-pointer function
+	// arguments, will be passed by value, not by reference. So the value
+	// that Choice2 and Prefix see is the original value of VMC (the closure
+	// of VMC over itself). That value is what Choice2 and Prefix capture
+	// in the closures that they create and return.
+	//
+	// After the next line of code runs, VMC will have a new value. That value
+	// is the more complex function that defines the complicated vending
+	// machine process. It is this new value of VMC that will be passed
+	// to the Interact function in main.
+	//
+	// The trick here is that this new value of VMC is the result of
+	// executing a closure over the old value of VMC, which itself is a
+	// closure over the variable VMC. So when the new value of VMC
+	// executes, it will ultimately reference not the old VMC, but the new
+	// VMC. And so we get the desired recursive definition.
+	//
+	// We could have avoided all this by passing a pointer to VMC instead
+	// of the value VMC. But where's the fun in that? And this version
+	// of VMC is closer in spirit to the original LISP program by Hoare.
 	VMC = Choice2(
 		"in2p", Choice2(
 			"large", VMC,
@@ -92,5 +129,8 @@ func main() {
 	Interact(Events{"in2p", "small", "out1p", "in1p", "in1p", "large",
 		"in1p", "small", "√"}, VMC)
 	Interact(Events{"in1p", "in1p", "in1p", "√"}, VMC)
+	Interact(Events{"in2p", "large", "in2p", "large", "in2p", "large", "√"},
+		VMC)
+	// Three 1p coins in a row lead to STOP
 	Interact(Events{"in1p", "in1p", "in1p", "in1p", "√"}, VMC)
 }
